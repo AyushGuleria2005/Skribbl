4459ee0217cd07a15270efa57ed78a0702ff9923
Ayush <ag13015002@gmail.com>
2025-07-31 18:02:47 +0530

    feat:canvas-line-draw

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Canvas from './components/Canvas'
 const App = () => {
   return (
     <div>
-      <Canvas width="800" height="500" style={{background:'lightblue'}} onClick={()=>alert('Canvas Clicked')}/>
+      <Canvas width="800" height="500" style={{background:'lightblue'}} />
     </div>
   )
 }
--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,27 +1,73 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Canvas = (props) => {
+  //Selecting the canvas element
   const ref = useRef();
+  const [isDrawing, setIsDrawing] = useState(false);
+  // const [posX,setPosX] = useState(null);
+  // const [posY,setPosY] = useState(null);
+  const posX = useRef(null);
+  const posY = useRef(null);
+  const startPosX = useRef(null);
+  const startPosY = useRef(null);
+  const endPosX = useRef(null);
+  const endPosY = useRef(null);
+
   useEffect(() => {
     const canvas = ref.current;
-    const context = canvas.getContext("2d");
-    console.log(context);     // CanvasRenderingContext2d
-    context.beginPath();
-    context.moveTo(50, 100);
-    context.lineTo(100, 200);
-    context.lineTo(50, 200);
-    context.closePath();
+    //Creating context i.e pencil to draw
+    const ctx = canvas.getContext("2d");
+    //Let's draw
+    if (
+      startPosX.current !== null &&
+      startPosY.current !== null &&
+      endPosX.current !== null &&
+      endPosY.current !== null
+    ) {
+      ctx.beginPath();
+      ctx.moveTo(startPosX.current, startPosY.current);
+      ctx.lineTo(endPosX.current, endPosY.current);
+      ctx.lineCap = "round";
+      ctx.strokeStyle = "red";
+      ctx.lineWidth = 5;
+      ctx.stroke();
+    }
+  }, [isDrawing]);
+  return (
+    <div>
+      <canvas
+        ref={ref}
+        {...props}
+        onMouseMove={(e) => {
+          posX.current = e.clientX;
+          posY.current = e.clientY;
 
-    context.fillStyle = "red";
-    context.fill();
-
-    context.strokeStyle = "black";
-    context.lineWidth = 2;
-    context.lineCap = 'round';
-    context.lineJoin = 'round';
-    context.stroke();
-  },[]);
-  return <canvas ref={ref} {...props} />;
+        }}
+        onMouseDown={() => {
+          // console.log("Point clicked is: "+posX+" and "+posY);
+          startPosX.current = posX.current;
+          startPosY.current = posY.current;
+          setIsDrawing(true);
+          // console.log(startPosX.current);
+          // console.log(startPosY.current);
+        }}
+        onMouseUp={() => {
+          // console.log("Point released is: "+posX+" and "+posY);
+          endPosX.current = posX.current;
+          endPosY.current = posY.current;
+          console.log(endPosX.current);
+          console.log(endPosY.current);
+          setIsDrawing(false);
+        }}
+      />
+    </div>
+  );
 };
 
 export default Canvas;
+
+// Click krne ke baad and hold that click ke baad we can only then use mousemove
+
+//mousedown => click
+//mousemove=> move my mouse
+//mouseup => when i release my click
