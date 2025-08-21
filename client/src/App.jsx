457320721beb427361ff9eb0016f02f30918dfc5
Ayush <ag13015002@gmail.com>
2025-08-21 14:10:48 +0530

    testing out something

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import Canvas from './components/Canvas'
-import Main from './components/Main'
-import './index.css'
+import React from "react";
+import Canvas from "./components/Canvas";
+import Main from "./components/Main";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+    },
+    {
+      path: "/canvas",
+      element: (
+        <Canvas
+          width="900"
+          height="500"
+          style={{
+            background: "#1C1C1C",
+            border: "2px solid black"
+          }}
+        />
+      ),
+    },
+  ]);
+
 
 const App = () => {
-  return (
-    <div>
-      <Main/>
-      {/* <Canvas width="900" height="500" style={{background:'#1C1C1C',border:'2px solid black',cursor:'url("/cursor/edit.png"), auto'}} /> */}
-    </div>
-  )
-}
+   return (
+        <RouterProvider router={appRouter}/>
+    )
+};
 
-export default App
+export default App;
